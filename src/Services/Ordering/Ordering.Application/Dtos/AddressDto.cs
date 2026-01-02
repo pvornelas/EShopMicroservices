edae7dbdf3c5eb09c714b95ea231d053440d500d
pvornelas <pviniciusornelas@gmail.com>
2026-01-02 20:14:30 -0300

    Criação das classes dto

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Dtos
+{
+    public record AddressDto(
+        string FirstName,
+        string LastName,
+        string EmailAdress,
+        string AddressLine,
+        string Country,
+        string State,
+        string ZipCode);
+}
---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Enum;
+
+namespace Ordering.Application.Dtos
+{
+    public record OrderDto(
+        Guid Id,
+        Guid CustomerId,
+        string OrderName,
+        AddressDto ShippingAddress,
+        AddressDto BillingAdress,
+        PaymentDto Payment,
+        OrderStatus Status,
+        List<OrderItemDto> OrderItems);
+}
